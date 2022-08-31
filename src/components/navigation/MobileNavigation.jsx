f05c6c1087d5e70b5ba3c170523af39c3237b9dc
IAmQpac <77878877+Jerjian@users.noreply.github.com>
2022-08-31 03:51:35 -0400

    navbar component finished

--- src/components/navigation/MobileNavigation.jsx
+++ src/components/navigation/MobileNavigation.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import logo from "../../assets/logo.png"
 import NavLinks from "./NavLinks"
+import {GiHamburgerMenu} from "react-icons/gi"
+import {AiOutlineClose} from  "react-icons/ai"
+
 
 export default function(){
+    const [burger, setBurger] =  React.useState(false)
+    function toggleBuger(){
+        setBurger(prevBurger => !prevBurger)
+    }
     return(
         <nav className="MobileNavigation">
             <img src={logo} alt="logo-img" className="nav-logo" />
-            <NavLinks />
+            {burger && <NavLinks />}
+            {burger ?<AiOutlineClose className="AiOutlineClose" onClick={toggleBuger}/> : <GiHamburgerMenu className="GiHamburgerMenu" onClick={toggleBuger} />}
         </nav>
     )
 
--- src/components/navigation/NavLinks.jsx
+++ src/components/navigation/NavLinks.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function(){
     return( 
         <div className="NavLinks">
-            <a href="">About</a>
-            <a href="">Work</a>
-            <a href="">Skills</a>
-            <a href="">Contact</a>
+            <a href="/#About" className="NavLinks-item">About</a>
+            <a href="/#Work" className="NavLinks-item">Work</a>
+            <a href="/#Skills" className="NavLinks-item">Skills</a>
+            <a href="/#Contact" className="NavLinks-item">Contact</a>
         </div>
 
     )
